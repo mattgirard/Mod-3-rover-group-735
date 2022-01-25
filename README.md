b2dee0f52dfff700db2e18f831d134bc76559dbf
Matt Girard <93295156+mattgirard@users.noreply.github.com>
2022-01-25 18:53:22 -0500

    I made a change doing whatever

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Mod-3-rover-group-735
 APSC module 3 mars rover navigation system
+
+Hello this is a change
--- testing.py
+++ testing.py
@@ -1,3 +1,5 @@
 #This is the initial python file for tesing
-
+import numpy
 print("this is a test")
+
+print("this is a change")
