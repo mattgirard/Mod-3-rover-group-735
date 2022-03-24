e3acab2c010784a7dd5b44fdd1dfe89cc0df483c
RayyanRamesh <rayyanramesh@gmail.com>
2022-03-24 12:45:27 -0400

    PIDupdate

--- PID.py
+++ PID.py
@@ -1,93 +1,106 @@
 import time
 
-#Define PID as dictionary of roll, pitch, yaw, and f)
-def PID(x, y, yaw, f):
-
+#Define PID as dictionary of x, y , and f)
+def PID(x, y, f):
+    global kprop_x, kint_x, kder_x, kprop_y, kint_y, kder_y, previousRun_xerror, previousRun_yerror, xMemoryForP, yMemoryForP, xMemoryForI, yMemoryForI, xMemoryForD, yMemoryForD, flag, setpoint, sampletime
 # Define PID gain values as global variables to prevent them from resetting to zero each time function is called
-#Needs PID gain values for pitch roll and yaw indivudally which are the rotations of the vehicle along the x,y,z axis respectivley
-#Consider if it is possible to only utlisze pitch and roll since yaw seems redundant as it is the z axis movement
+#Needs PID gain values for roll and pitch indivudally which are the rotations of the vehicle along the x and yaxis respectivley
 
-kprop_y = 3 
-kint_y=3 
-kder_y = 3
-kprop_x = 3
-kint_x =3
-kder_x = 3
+    #Current gain values are arbitrary
+    kprop_x = 80
+    kint_x = 0.002
+    kder_x = 89
+    kprop_y = kprop_x
+    kint_y= kint_x
+    kder_y = kder_x
 #kprop_yaw =0.1
 #kint_yaw = 0
 #kder_yaw = 0
-flag = 0
+    flag = 0
 #Other variables below
 #For sample time we can start with 0 but it mat be benefical to vary this during and as a result of testing
-sampletime = 0 
-setpoint = 0
+    sampletime = 0 
+    setpoint = 0
 #Need to find error of pitch roll and yaw
-yerror = float(pitch)*(180/pi) - setpoint
-xerror = float(roll)*(180/pi) - setpoint
+    xerror = float(x)*(180/3.141592653) - setpoint
+    yerror = float(y)*(180/3.141592653) - setpoint
+
 #yawerror = float(yaw)*(180/pi) - setpoint
 
 #Not sure why currTime is time.time() yet 
-currenttime = time.time()
+    currenttime = time.time()
 
 #----------------------------------------------------------------------------------------
 #These following variables are reset during the first loop of the controller only
 
-if flag == 0:
-    previousRunTime = 0
-    previousRunyerror = 0
-    previousRunxerror = 0
+    if flag == 0:
+        previousRun_Time = 0
+        previousRun_xerror = 0
+        previousRun_yerror = 0
+    
 #previousRunyawerror = 0
 
-    yMemoryForP = 0
-    xMemoryForP = 0
+        xMemoryForP = 0
+        yMemoryForP = 0
+   
 #yawMemoryForP = 0
 
-    yMemoryForI = 0
-    xMemoryForI = 0
+        xMemoryForI = 0
+        yMemoryForI = 0
+    
 #yawMemoryForI = 0
 
-    yMemoryForD = 0
-    xMemoryForD = 0
+        xMemoryForD = 0
+        yMemoryForD = 0
+    
 #yawMemoryForD = 0
 
 # Adds a value and the variable and assigns the result to that variable
-    flag += 1
+        flag += 1
 #-----------------------------------------------------------------------------------------------
 #dt, dy(t) must be defined to be able to be used in the D term of the PID controller 
 
-dtime = currenttime - previousRunTime
-dyerror = yerror - previousRunyerror
-dxerror = xerror - previousRunxerror
+    dtime = currenttime - previousRunTime
+
+    dxerror = xerror - previousRun_xerror
+    dyerror = yerror - previousRun_yerror
+
 #dyawerror = yawerror - previousRunyawerror
 
 #"Heart of PID alg". the alg will be more accurately if it is sampled at regular intervals. Sample time can be changed to optimize controller
-if (dtime >= sampletime):
+    if (dtime >= sampletime):
     #Following is proportional term 
-    yMemoryForP = kprop_y * yerror
-    xMemoryForP = kprop_x * xerror
+
+        xMemoryForP = kprop_x * xerror
+        yMemoryForP = kprop_y * yerror
+    
     #yawMemoryForP =   #Still not sure if Yaw is needed but can be easily added if necessary
 
     #Integral term
-    yMemoryForI += yerror * dtime
-    xMemoryForI += xerror * dtime
+        xMemoryForI += xerror * dtime
+        yMemoryForI += yerror * dtime
+    
     #yawMemoryForI += yawerror * dtime
 
-    if(yMemoryForI > 400): yMemoryForI = 400
-    if(yMemoryForI < -400): yMemoryForI = -400
-    if(xMemoryForI > 400): xMemoryForI = 400
-    if(xMemoryForI < -400): xMemoryForI = -400
+        if(xMemoryForI > 400): xMemoryForI = 400
+        if(xMemoryForI < -400): xMemoryForI = -400
+        if(yMemoryForI > 400): yMemoryForI = 400
+        if(yMemoryForI < -400): yMemoryForI = -400
+
     #if(yawMemoryForI > 400): yawMemoryForI = 400
     #if(yawMemoryForI < -400): yawMemoryForI = -400
 
     #Derivative term
-    yMemoryForD = dyerror/dtime
+
     xMemoryForD = dxerror/dtime
+    yMemoryForD = dyerror/dtime
+    
     #yawMemoryForD = dyawerror/dtime
 
 #Out of if statememnt store variables from most recent run into the previous variables to utilise in the next iterations of the loop
-previousRunTime = currenttime
-previousRunxerror = xerror
-previousRunyerror = yerror
+    previousRunTime = currenttime
+    previousRun_xerror = xerror
+    previousRun_yerror = yerror
 #previousRunyawerror = yawerror
 
 #Outputs based on collective PID equation for pitch roll and yaw
